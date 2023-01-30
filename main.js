99354320f2b4d74912e1d8f03646ca7e1871d624
staceymw <116839832+staceymw@users.noreply.github.com>
2023-01-30 11:09:14 -0500

    Create JS file and write function to calculate and display

---
+++ main.js
@@ -0,0 +1,50 @@
+
+function operate (operator, a, b) {
+    switch(operator) {
+        case "add":
+            display.textContent = a + b
+            break;
+        case "subtract":
+            display.textContent = a - b
+            break;
+        case "multiply":
+            display.textContent = a * b
+            break;
+        case "divide":
+            display.textContent = a / b
+            break;       
+    }
+};            
+                   
+                   
+                   
+                    /*function add (a, b) {
+                        return a + b;
+                    };
+                    
+                    function subtract (a, b) {
+                        return a - b;
+                    };
+                    
+                    function multiply (a, b) {
+                        return a * b;
+                    };
+                    
+                    function divide (a, b) {
+                        return a / b;
+                    };
+
+
+                    function operate (operator a, b)
+                        operator = operation;
+                        a = firstNumber;
+                        b = secondNumber;
+                        if (operator == "+") {
+                            return result = add(a, b);
+                        } else if (operator == "-") {
+                            return result = subtract(a,b);
+                        } else if (operator == "*") {
+                            return result = multiply(a, b);
+                        } else {
+                            return result = divide(a, b);
+                        };
