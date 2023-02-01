ec8e54ab86844b09acbe39bc4f09f514fcb3e1e0
staceymw <116839832+staceymw@users.noreply.github.com>
2023-02-01 12:50:37 -0500

    Add event listeners to buttons, initialize values, create functions to append, handle operations and compute

--- main.js
+++ main.js
@@ -21,14 +21,14 @@ function divide (a, b) {
 
 function percent (a, b) {
     return a  * (b / 100);
-}
+};
 
 
-function operate (operator, a, b) {
+function operate (op, a, b) {
     let result = 0;
-    a = num1;
-    b = num2;
-    switch(operator) {
+    a = Number(a);
+    b = Number(b);
+    switch(op) {
         case "add":
             result = add(a, b)
             break;
@@ -47,4 +47,114 @@ function operate (operator, a, b) {
                 break;       
     }
     return result;
+};
+
+const numberButtons = document.querySelectorAll(".number");
+const opButtons = document.querySelectorAll(".operation");
+const equals = document.querySelector(".equals");
+const clear = document.querySelector(".clear");
+const previous = document.querySelector(".previous");
+const current = document.querySelector(".current");
+
+let currentOp = "";
+let displayValue = "";
+let previousVal = "";
+let currentVal = "";
+let computed = false;
+
+numberButtons.forEach(button => {
+    button.addEventListener("click", () => {
+        appendNumber(button.innerText);
+        updateDisplay();
+    })
+});
+
+opButtons.forEach(button => {
+    button.addEventListener("click", () => {
+        appendNumber(button.innerText);
+        updateDisplay();
+    })
+});
+
+equals.addEventListener("click", () => {
+    compute();
+});
+
+clear.addEventListener("click", () => {
+    clearAll();
+});
+
+function appendNumber(number) {
+    if (number === "." && current.innerText.includes(".")) {
+    return;
+}
+    if (computed) {
+        currentVal = "";
+        displayVal = "";
+        current.innerText = "";
+        computed = false
+    }
+    displayValue += number;
+    if (number !== ".") {
+        currentVal += number
+    }
+};
+
+function updateDisplay() {
+    current.innerText = displayValue;
+    currentVal = +current.innerText;
+};
+
+function compute() {
+    if (currentOp !== "") {
+        console.log(previousVal, currentVal);
+        let result = operate(currentOP, previousVal, currentVal);
+        if (result || result === 0) {
+            if (result.toString().length > 8) {
+                current.innerText = result.toFixed(6);
+                currentVal = result.toFixed(6);
+            } else {
+                current.innerText = result;
+                currentVal = result;
+            }
+            previousVal = "";
+            previous.innerText = "";
+            computed = true;
+            currentOp = "";
+        }
+    }
+}
+
+function handleOperator(op) {
+    if (currentOp && !current.innerText) {
+        return
+    }
+    if (previous.innerText && current.innerText) {
+    let newOp = previous.innerText(previous.innerText.length - 1);
+    let result = operate(newOp, previousVal, currentVal);
+    previous.innerText = `${result}${op}`;
+    previousVal = result;
+    currentOp = op;
+    current.innerText = "";
+    currentVal = "";
+    displayVal = "";
+    return;
+    }
+    
+    previous.innerText = current.innerText;
+    previousValue = currentValue;
+    currentOp = op;
+    current.innerText = "";
+    currentVal = "";
+    previous.innerText += `${op}`;
+    displayValue = "";
+}
+
+function clearAll() {
+    displayValue = "";
+    previousValue = "";
+    currentValue = "";
+    currentOp = "";
+    previous.innerText = "";
+    displayValue = "";
 }
